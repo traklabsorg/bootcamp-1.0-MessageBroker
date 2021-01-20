c9ec0feff3a01fef648478ad9386e49a109cf385
debu3223 <debabratamukherjee@trakinvest.com>
2021-01-20 11:07:30 +0530

    Console for aws credentials Added

--- SNS_SQS.ts
+++ SNS_SQS.ts
@@ -20,6 +20,12 @@ AWS.config.update({
   region: process.env.AWS_REGION,
 });
 
+console.log({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
 var sns = new AWS.SNS({ apiVersion: "2010-03-31" });
 var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
 
